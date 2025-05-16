506ba78bd4b9cd9333f433c5ac806997e52c2f9d
Rodrigo-Carinelli <rodrigo.carinelli@hotmail.com>
2025-05-16 04:00:27 -0300

    terminando la pagina

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,13 +2,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Item = ({ product }) => {
+const Item = ({ producto }) => {
 return (
     <div className="item-card">
-    <h3>{product.nombre}</h3>
-    <img src={product.imagen} alt={product.nombre} style={{ width: "200px" }} />
-    <p>Precio: ${product.precio}</p>
-    <Link to={`/item/${product.id}`}>Ver detalle</Link>
+    <h3>{producto.nombre}</h3>
+    <img src={producto.imagen} alt={producto.nombre} style={{ width: "200px" }} />
+    <p>Precio: ${producto.precio}</p>
+    <Link to={`/item/${producto.id}`}>Ver detalle</Link>
     </div>
 );
 };
